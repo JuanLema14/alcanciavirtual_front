d134e209c7ad845cd4c3878e139d95b81dd013f2
JuanLema14 <72539768+JuanLema14@users.noreply.github.com>
2023-09-29 15:31:02 -0500

    essentialLinks y Main Layout

--- src/components/EssentialLink.vue
+++ src/components/EssentialLink.vue
@@ -1,49 +1,97 @@
 <template>
   <q-item
     clickable
-    tag="a"
-    target="_blank"
-    :href="link"
+    autofocus
+    @click="irAlLink(link)"
+    style="margin: 4%; border-radius: 5px; width: 76%"
+    :class="claseLinkSeleccionado"
   >
-    <q-item-section
-      v-if="icon"
-      avatar
-    >
-      <q-icon :name="icon" />
+    <q-item-section v-if="icon" avatar>
+      <div class="row flex flex-center">
+        <q-avatar
+          style="width: 2.3vmax; height: 2.3vmax"
+          rounded
+          :icon="icon"
+          :text-color="iconoColor"
+          :class="validarClick(link)"
+        />
+      </div>
     </q-item-section>
-
     <q-item-section>
-      <q-item-label>{{ title }}</q-item-label>
-      <q-item-label caption>{{ caption }}</q-item-label>
+      <q-item-label :class="textoColorClase">{{ text }}</q-item-label>
+      <q-item-label v-if="icon" caption>
+        {{ caption }}
+      </q-item-label>
     </q-item-section>
   </q-item>
 </template>
 
-<script>
-import { defineComponent } from 'vue'
+<script setup>
+import { useRoutesStore } from "stores/routes-store.js";
+import { useRouter } from "vue-router";
+import { ref } from "vue";
 
-export default defineComponent({
-  name: 'EssentialLink',
-  props: {
-    title: {
-      type: String,
-      required: true
-    },
+const routesStore = useRoutesStore();
+const router = useRouter();
+const props = defineProps({
+  text: {
+    type: String,
+    required: true,
+  },
 
-    caption: {
-      type: String,
-      default: ''
-    },
+  caption: {
+    type: String,
+    default: "",
+  },
 
-    link: {
-      type: String,
-      default: '#'
-    },
+  link: {
+    type: String,
+    default: "#",
+  },
 
-    icon: {
-      type: String,
-      default: ''
+  icon: {
+    type: String,
+    default: "",
+  },
+
+  color: {
+    type: String,
+    default: "grey",
+  },
+});
+
+const iconoColor = ref("secondary");
+const textoColorClase = ref("text-secondary");
+const claseLinkSeleccionado = ref("");
+
+function validarClick(link) {
+  if (routesStore.linkActualMenu.length) {
+    if (routesStore.linkActualMenu.includes(link)) {
+      iconoColor.value = "white";
+      claseLinkSeleccionado.value = "shadowBox bg-white flat";
+      textoColorClase.value = "text-secondary";
+      return "bg-primary text-white shadow-1";
+    } else {
+      claseLinkSeleccionado.value = "";
+      textoColorClase.value = "text-grey-6";
+      iconoColor.value = "secondary";
+      return "shadowBox";
     }
+  } else {
+    return "shadowBox";
   }
-})
+}
+
+async function irAlLink(link) {
+  routesStore.linkActualMenu = [];
+  routesStore.linkActualMenu.push(link);
+  router.push(link);
+}
 </script>
+
+<style scoped>
+.shadowBox {
+  -webkit-box-shadow: -1px 14px 24px -5px rgba(57, 66, 196, 0.54);
+  box-shadow: -1px 14px 24px -5px rgba(11, 19, 138, 0.54);
+}
+</style>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,42 +1,99 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
+  <q-layout view="lHh Lpr lFf" class="bg">
+    <q-header flat class="bg-transparent q-pa-md">
+      <q-toolbar class="toolbarShadow" style="border-radius: 10px 10px 10px 10px">
+        <q-btn flat dense round text-color="grey-7" icon="menu" aria-label="Menu" @click="toggleLeftDrawer"
+          v-if="!leftDrawerOpen" />
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
+        <q-space />
 
-        <div>Quasar v{{ $q.version }}</div>
+        <div class="q-pl-sm q-gutter-sm row items-center no-wrap flex">
+          <div class="row flex flex-center absolute" style="right: 1vmax; top: -0.3vmax">
+            <span v-if="morphGroupModel === 'btn'" class="text-grey-7">Opciones de usuario</span>
+            <q-btn v-morph:btn:userGroup:300.resize="morphGroupModel" fab flat color="grey-7" icon="person"
+              @click="nextMorph">
+            </q-btn>
+          </div>
+
+          <q-card v-morph:card1:userGroup:500.resize="morphGroupModel" class="bg-white settingsMenu" flat>
+            <q-card-section class="sectionShadow">
+              <div class="row justify-between flex flex-center">
+                <span class="text-grey-7">{{ email }}</span>
+                <q-btn fab flat color="grey-7" icon="person" @click="nextMorph" />
+              </div>
+
+              <q-separator />
+
+              <q-item clickable @click="logout">
+                <q-item-section>
+                  <div class="text-secondary">
+                    <q-icon name="logout" color="grey-7" />
+                    Salir
+                  </div>
+                </q-item-section>
+              </q-item>
+              <q-btn flat label="Cancel" text-color="secondary" @click="nextMorph" />
+            </q-card-section>
+          </q-card>
+        </div>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
+    <q-drawer v-model="leftDrawerOpen" style="background: #f2f6ff;" show-if-above>
+      <q-scroll-area class="fit">
+        <div class="q-pt-md q-pb-md">
+          <q-btn flat no-caps no-wrap class="q-ml-xs" to="/main">
+            <div class="flex flex-center">
+              <q-avatar square text-color="white" style="width: 2.3vmax; height: 2.3vmax">
+                <div style="z-index: 1000;">
+                  <div class="row flex flex-center">
+                    <img src="~assets/logo.png" style="width: 60%;" />
+                  </div>
+                </div>
+                <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg"
+                  style="width: 100%; z-index: 0; position: absolute;">
+                  <path fill="#F5ACFF"
+                    d="M42.7,-77.8C53.2,-68,57.8,-52.1,63.2,-38.1C68.5,-24.1,74.4,-12,73.1,-0.8C71.8,10.5,63.2,21,56.2,32.2C49.3,43.4,44.1,55.3,35,65.6C25.9,75.9,12.9,84.6,1,82.8C-10.9,81,-21.8,68.9,-31.1,58.7C-40.4,48.5,-48.1,40.4,-57.4,30.9C-66.8,21.5,-77.8,10.7,-81,-1.9C-84.2,-14.5,-79.7,-28.9,-72.7,-42.5C-65.8,-56,-56.4,-68.7,-43.8,-77.2C-31.3,-85.8,-15.6,-90.2,0.3,-90.6C16.2,-91.1,32.3,-87.5,42.7,-77.8Z"
+                    transform="translate(100 100)" />
+                </svg>
+              </q-avatar>
+            </div>
+            <q-toolbar-title shrink class="text-weight-bold flex flex-center">
+              <span class="text-secondary" style="font-size: 1.8vh">Alcancia Virtual</span>
+            </q-toolbar-title>
+          </q-btn>
+        </div>
 
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
+        <q-item>
+          <q-item-section>
+            <q-separator class="q-mt-md q-mb-xs" color="grey-4" />
+          </q-item-section>
+        </q-item>
+
+        <q-list padding>
+          <div class="text-primary q-pl-lg q-pt-md" v-if="links1.length > 0">
+            OPCIONES
+          </div>
+          <EssentialLink v-for="link in links1" :key="link.text" v-bind="link" />
+
+          <q-item>
+            <q-item-section>
+              <q-separator v-if="links2.length > 0" class="q-mt-md q-mb-xs" color="grey-4" />
+            </q-item-section>
+          </q-item>
+
+          <EssentialLink v-for="link in links2" :key="link.text" v-bind="link" />
+
+          <q-item>
+            <q-item-section>
+              <q-separator v-if="links2.length > 0" class="q-mt-md q-mb-xs" color="grey-4" />
+            </q-item-section>
+          </q-item>
+
+          <EssentialLink v-for="link in links3" :key="link.text" v-bind="link" />
+        </q-list>
+
+      </q-scroll-area>
     </q-drawer>
 
     <q-page-container>
@@ -45,72 +102,148 @@
   </q-layout>
 </template>
 
-<script>
-import { defineComponent, ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
+<script setup>
+import { ref, defineAsyncComponent, computed, onMounted } from "vue";
+import { useAuthStore } from "stores/auth-store";
+import { useRoutesStore } from "stores/routes-store.js";
+import { useQuasar } from "quasar";
+import { useRouter } from "vue-router";
 
-const linksList = [
+const EssentialLink = defineAsyncComponent(() =>
+  import("../components/EssentialLink.vue")
+);
+
+const router = useRouter();
+const $q = useQuasar();
+const leftDrawerOpen = ref(true);
+const autenticacionStore = useAuthStore();
+const routesStore = useRoutesStore();
+const morphGroupModel = ref("btn");
+
+const nextMorphStep = {
+  btn: "card1",
+  card1: "btn",
+};
+
+function nextMorph() {
+  morphGroupModel.value = nextMorphStep[morphGroupModel.value];
+}
+
+const links1 = ref([
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
+    text: 'Home',
+    caption: 'home',
+    icon: 'home',
+    link: '/main'
   },
   {
-    title: 'Github',
+    text: 'Github',
     caption: 'github.com/quasarframework',
     icon: 'code',
-    link: 'https://github.com/quasarframework'
+    link: '/'
   },
   {
-    title: 'Discord Chat Channel',
+    text: 'Discord Chat Channel',
     caption: 'chat.quasar.dev',
     icon: 'chat',
-    link: 'https://chat.quasar.dev'
+    link: '/'
   },
   {
-    title: 'Forum',
+    text: 'Forum',
     caption: 'forum.quasar.dev',
     icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
+    link: '/'
   },
   {
-    title: 'Twitter',
+    text: 'Twitter',
     caption: '@quasarframework',
     icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
+    link: '/'
   },
   {
-    title: 'Facebook',
+    text: 'Facebook',
     caption: '@QuasarFramework',
     icon: 'public',
-    link: 'https://facebook.quasar.dev'
+    link: '/'
   },
   {
-    title: 'Quasar Awesome',
+    text: 'Quasar Awesome',
     caption: 'Community Quasar projects',
     icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
+    link: '/'
   }
-]
+])
 
-export default defineComponent({
-  name: 'MainLayout',
-
-  components: {
-    EssentialLink
-  },
-
-  setup () {
-    const leftDrawerOpen = ref(false)
-
-    return {
-      essentialLinks: linksList,
-      leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
+const links2 = ref([])
+const links3 = ref([])
+const email = computed({
+  get() {
+    if (autenticacionStore.responseAuth != null) {
+      return autenticacionStore.email;
+    } else {
+      return "";
     }
-  }
-})
+  },
+});
+const nombre = computed({
+  get() {
+    if (autenticacionStore.responseAuth != null) {
+      return autenticacionStore.nombre;
+    } else {
+      return "";
+    }
+  },
+});
+
+onMounted(() => {
+  routesStore.linkActualMenu = [];
+  routesStore.linkActualMenu.push(router.currentRoute.value.path);
+});
+
+function logout() {
+  $q.loading.show({
+    message: "Saliendo ...",
+  });
+  autenticacionStore
+    .logout()
+    .then(() => {
+      routesStore.linkActualMenu = [];
+      router.push("/");
+    })
+    .finally(() => {
+      $q.loading.hide();
+    });
+}
+
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+}
 </script>
+
+<style lang="scss" scoped>
+.bg {
+  background-color: #f2f6ff;
+}
+
+.textoEmail {
+  font-size: 0.8vmax;
+}
+
+.settingsMenu {
+  width: 300px;
+  border-radius: 2em;
+  right: 1vmax;
+  top: 0vmax;
+  position: absolute;
+}
+
+.sectionShadow {
+  -webkit-box-shadow: -1px 14px 24px -5px rgba(57, 66, 196, 0.5);
+  box-shadow: -1px 14px 24px -5px rgba(11, 19, 138, 0.2);
+}
+
+.toolbarShadow {
+  background: #fff;
+  box-shadow: 0 .3125rem .625rem #0000001f !important;
+}
+</style>
